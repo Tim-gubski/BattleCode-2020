be6b3b3e5b16fccef71e237bdcd5e5beb1ea58cf
SteamBlizzard <SteamBlizzard@users.noreply.github.com>
2020-01-15 18:21:05 -0700

    Update RobotPlayer.java

--- TimsPlayer/RobotPlayer.java
+++ TimsPlayer/RobotPlayer.java
@@ -76,7 +76,7 @@ public strictfp class RobotPlayer {
         //FIX
         
         
-        if (hqLoc != null && hqLoc.x >= 0 && hqLoc.y >= 0) {
+
             bigRadius[0] = new MapLocation(hqLoc.x, hqLoc.y + 2);
             bigRadius[1] = new MapLocation(hqLoc.x + 1, hqLoc.y + 2);
             bigRadius[2] = new MapLocation(hqLoc.x + 2, hqLoc.y + 2);
@@ -93,7 +93,6 @@ public strictfp class RobotPlayer {
             bigRadius[13] = new MapLocation(hqLoc.x - 2, hqLoc.y + 1);
             bigRadius[14] = new MapLocation(hqLoc.x - 2, hqLoc.y + 2);
             bigRadius[15] = new MapLocation(hqLoc.x - 1, hqLoc.y + 2);
-        }
         
         //FIX
         //FIX
